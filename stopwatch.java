899779a383cad4e86fd845475ce5a688482765fa
Kun@@l <72121803+Kunaal-Mahajan@users.noreply.github.com>
2022-09-03 12:31:30 +0530

    Update stopwatch.java

--- stopwatch.java
+++ stopwatch.java
@@ -292,7 +292,7 @@ public class stopwatch extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new stopwatch().setVisible(true);
